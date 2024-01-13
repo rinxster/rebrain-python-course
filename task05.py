4abf2ffadfbf603e87c2917eba5288882eaf777a
rinxster <sark@bk.ru>
2024-01-13 12:51:11 +0300

    задача 5

--- task05.py
+++ task05.py
@@ -11,7 +11,6 @@
 # May 23 08:06:14 PC-00233 kernel: [221559.381614] usbcore: registered new interface driver snd-usb-audio
 # May 24 16:19:52 PC-00233 systemd[1116]: Reached target Sound Card.
 # May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users.
-
 list1 = [
 'May 18 11:59:18 PC-00102 plasmashell[1312]: kf.plasma.core: findInCache with a lastModified timestamp of 0 is deprecated',
 'May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations.',
@@ -26,29 +25,29 @@ list1 = [
 'May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users.'
 ]
 # print(len(list1))
-for i in range(0, len(list1)):
-    print(i)
-    string2 = list1[i]
-    x = string2.split(" ")
-    # 'time': <дата/время>
-    time2 = ''
-    for i in range(3):
-        time2 = str(time2) + x[0] + ' '
-        x.pop(0)
-
-    # 'pc_name': <имя компьютера>
-    pc_name = x[0]
-    x.pop(0)
-    # 'service_name': <имя сервиса>
-    service_name = x[0][:-1]
-    x.pop(0)
-    # 'message': <сообщение лога>
-    separator = ' '
-    message = separator.join(x)
-    my_information2 = dict({'time': time2, 'pc_name': pc_name, 'service_name': service_name, 'message': message})
-    print('----# <имя компьютера>: <сообщение>---------')
-    print(my_information2['pc_name'] + ' : ' + my_information2['message'])
-
+# for i in range(0, len(list1)):
+#     print(i)
+#     string2 = list1[i]
+#     x = string2.split(" ")
+#     # 'time': <дата/время>
+#     time2 = ''
+#     for i in range(3):
+#         time2 = str(time2) + x[0] + ' '
+#         x.pop(0)
+#
+#     # 'pc_name': <имя компьютера>
+#     pc_name = x[0]
+#     x.pop(0)
+#     # 'service_name': <имя сервиса>
+#     service_name = x[0][:-1]
+#     x.pop(0)
+#     # 'message': <сообщение лога>
+#     separator = ' '
+#     message = separator.join(x)
+#     my_information2 = dict({'time': time2, 'pc_name': pc_name, 'service_name': service_name, 'message': message})
+#     print('----# <имя компьютера>: <сообщение>---------')
+#     print(my_information2['pc_name'] + ' : ' + my_information2['message'])
+#
 
 # 2. Создайте из него список словарей, используя ключи из того же задания. Напоминаю:
 # 'time': <дата/время>
@@ -56,8 +55,21 @@ for i in range(0, len(list1)):
 # 'service_name': <имя сервиса>
 # 'message': <сообщение лога>
 
+# {'time': <дата/время>, 'pc_name': <имя компьютера>, 'service_name': <имя сервиса>, 'message': <сообщение лога>}
+
 # //TODO XXX
 
+#https://builtin.com/software-engineering-perspectives/convert-list-to-dictionary-python
+# используем функцию ZIP()
+#сделать список ключей
+
+#сделать список значений(сначала для одной строки)
+l1=['time', 'pc_name', 'service_name', 'message']
+
+# для примера сделать для одной струки списк ключей и список значений
+
+# сделать цикл построчно склеивать списк ключей и список значений
+l2=
 
 # 3. Выведите на экран список значений <дата/время> всех словарей. Воспользуйтесь списковым включением.
 
@@ -85,9 +97,13 @@ for i in range(0, len(list1)):
     print(my_information2['time'])
 
 # 4. Измените словари в списке: создайте новый ключ 'date', перенеся в его значение дату из поля 'time'. В поле 'time' оставьте только время. Выведите значения для поля 'time' всех словарей в списке.
+# //TODO XXX
 
 # 5. Выведите список значений поля 'message' для всех логов, которые записал ПК с именем 'PC0078'. Воспользуйтесь списковым включением.
+# //TODO XXX
 
 # 6. Превратите список словарей логов (который вы сделали в задании 2) в словарь. Ключами в нем будут целые числа от 100 до 110, а значениями - словари логов.
+# //TODO XXX
 
 # 7. Выведите на экран словарь лога под ключом 104.
+# //TODO XXX
